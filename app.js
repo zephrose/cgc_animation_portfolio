be9883bd6af221837e7abfde5f4dde24c365a8f2
Zephrose <zephrose@gmail.com>
2020-01-30 15:29:23 -0800

    Update port

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ const _ = require("lodash");
 
 const app = express();
 
+// Port
+let port = process.env.PORT;
+if (port == null || port == "") { port = 3000; }
+
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
@@ -31,6 +35,6 @@ app.get("/gallery", function(req, res) {
 
 
 // Where to listen
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+app.listen(port, function() {
+  console.log("Server started on " + port);
 });
